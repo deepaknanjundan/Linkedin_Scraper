3f12bf555f9755203045637ae79dd95aab3ae1c1
deepaknanjundan <57086435+deepaknanjundan@users.noreply.github.com>
2021-11-19 03:29:38 +0530

    Update CompanyScraper.py

--- scrape_linkedin/CompanyScraper.py
+++ scrape_linkedin/CompanyScraper.py
@@ -1,5 +1,3 @@
-import logging
-
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,8 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from .Company import Company
 from .Scraper import Scraper
 from .utils import AnyEC
-
-logger = logging.getLogger(__name__)
 
 
 class CompanyScraper(Scraper):
